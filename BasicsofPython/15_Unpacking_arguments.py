2b8c71e1b59dab3cab12c64383bdd0d41f7dd9f4
learn-modern-technology <mailmeatss2112@gmail.com>
2022-12-26 23:04:11 +0530

    Updated sample code

--- BasicsofPython/15_Unpacking_arguments.py
+++ BasicsofPython/15_Unpacking_arguments.py
@@ -12,19 +12,28 @@ product = multiply(1, 3, 5)
 print(f"Product is {product}")
 
 def add(x, y):
+    print("x - ",x)
+    print("y - ",y)
     return x + y
 
 number = [10, 20]
 sum_of_num = add(*number)
 print(f"sum of numbers {number} is {sum_of_num}")
 
+number2 = [40, 50]
+sum_of_num2 = add(*number2)
+print(f"number2 -", number2)
+print(f"number2 -", *number2)
+print(f"sum of numbers {number2} is {sum_of_num2}")
+
 nums = {"x": 25, "y": 42}
+#print(f"nums -", nums)
+print(f"nums -", *nums)
 sumofxy = add(**nums)
 print(f"sum of numbers {nums} is {sumofxy}")
 
 def apply(*args, operator):
     if operator == "*":
-#        return multiply(args)
         return multiply(*args)
     elif operator == "+":
         return sum(args)
@@ -36,3 +45,18 @@ print(f"Added value is - {added_value}")
 
 multiplied_val = apply(1,2,3,4,5,operator = "*")
 print(f"Multiplied value is - {multiplied_val}")
+
+
+def add2(a, b, c, d, e):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+    return a + b + c + d + e
+
+nums = {"a": 25, "b": 42, "c": 17, "d": 73, "e": 67}
+print(f"nums -", nums)
+print(f"nums -", *nums)
+sumofxy = add2(**nums)
+print(f"sum of numbers {nums} is {sumofxy}")
